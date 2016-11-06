e6f9b225b55e6992852a1cca4c54668cbff19a28
GabrielXia <Ga.Xiajin@gmail.com>
2016-11-06 22:55:09 +0100

    For question 188, to economy memory, using a table instead of a matrix

--- 188.BestTimeToBuyAndSellStock4/src/Solution.java
+++ 188.BestTimeToBuyAndSellStock4/src/Solution.java
@@ -1,28 +1,31 @@
 public class Solution {
+	//to economy memory, use a table instead of a matrix
     public int maxProfit(int k, int[] prices) {
         int size = prices.length;
         if(size<2)return 0;
         if(k<1)return 0;
         
-        int[][] f = new int[k][size];
-        f[0][0] = 0;
+        int[] f = new int[size];
+        f[0] = 0;
         int priceMin = prices[0];
         int nTimesBuyAmount;
         int i,j;
+        int tmp;
         for(i=1;i<size;i++){
-        	f[0][i] = Math.max(f[0][i-1], prices[i]-priceMin);
+        	f[i] = Math.max(f[i-1], prices[i]-priceMin);
         	priceMin = Math.min(priceMin, prices[i]);
         }
         
         for(i = 1;i < k ;i++){
-        	if(2*i>=size) return f[i-1][size-1];
-        	for(j = 0;j<=2*i;j++)f[i][j] = f[i-1][j];
-        	nTimesBuyAmount = f[i-1][2*i] - prices[2*i];
+        	if(2*i>=size) return f[size-1];
+        	
+        	nTimesBuyAmount = f[2*i] - prices[2*i];
         	for(j = 2*i+1; j<size;j++){
-        		f[i][j] = Math.max(f[i][j-1], prices[j]+nTimesBuyAmount);
-        		nTimesBuyAmount = Math.max(nTimesBuyAmount,f[i-1][j]-prices[j]);
+        		tmp = f[j];//tmp to remember the f[j] before changed, that is f[j] for j-1 transaction
+        		f[j] = Math.max(f[j-1], prices[j]+nTimesBuyAmount);
+        		nTimesBuyAmount = Math.max(nTimesBuyAmount,tmp-prices[j]);
         	}
         }
-        return f[k-1][size-1];
+        return f[size-1];
     }
 }
